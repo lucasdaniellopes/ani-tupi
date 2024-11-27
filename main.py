1ddee4e71d1a9023d3b3243a6fe36439b6ee21c4
Eduardo Nery <eduardo.nery8@gmail.com>
2024-11-27 08:20:13 -0300

    calling right repository method

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-import repository
 import loader
 from menu import menu
+from repository import rep
+from loader import PluginInterface
 
 if __name__=="__main__":
     loader.load_plugins(["animefire"], {"pt-br"})
     
     query = input("Pesquise anime: ")
-    
-    for source in repository.sources:
+    for source in rep.get_sources():
         source.search_anime(query)
-
-    selected_title = menu(repository.anime_titles) 
-    title_urls = repository.anime_titles_urls[repository.anime_titles.index(selected_title)]
-
-    for source in repository.sources:
-        source.get_episodes(title_urls)
-
-    selected_episode = menu(repository.episode_titles, "Escolha o episódio")
-    episode_url = repository.episode_urls[repository.episode_titles.index(selected_episode)]
-
-    for source in repository.sources:
-        source.find_player_source()
-
-    repository.play_episode()
